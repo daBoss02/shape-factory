49ceee6a0de91755f4f45a1a59d345f1bf7d9b33
Seth Vandenbos <seth.vdbos@gmail.com>
2022-11-22 09:31:34 -0600

    initial commit

---
+++ assets/script/index.js
@@ -0,0 +1,21 @@
+'use-strict';
+
+/*
+  Seth Vandenbos
+
+  Alarm Clock
+*/
+
+// Utility Functions
+function onEvent(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
+
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+function selectAll(selector, parent = document) {
+  return parent.querySelectorAll(selector);
+}
+
